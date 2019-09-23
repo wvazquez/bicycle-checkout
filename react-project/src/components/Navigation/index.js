b152f53c8ad8972373561e7400b102a83d0962f3
wvazquez <vazquezwilmer@gmail.com>
2019-09-23 17:45:29 -0400

    Fixes navigation menu.

--- react-project/src/components/Navigation/index.js
+++ react-project/src/components/Navigation/index.js
@@ -1,233 +1,124 @@
-// import React from 'react';
-
-// const Navigation = ()=>(
-//     <nav className="navbar navbar-expand-lg px-4">
-//       <a className="navbar-brand" href="#">Bicycle Rental</a>
-//       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav">
-//         <span className="toggler-icon"><i className="fas fa-bars"></i></span>
-//       </button>
-//       <div className="collapse navbar-collapse" id="myNav">
-//         <ul className="navbar-nav mx-auto text-capitalize">
-//           <li className="nav-item active">
-//             <a className="nav-link" href="#">home</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link " href="#">about</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">store</a>
-//           </li>
-//         </ul>
-//         <div className="nav-info-items d-none d-lg-flex ">
-//           <div className="nav-info align-items-center d-flex justify-content-between mx-lg-5">
-//             <span className="info-icon mx-lg-3"><i className="fas fa-phone"></i></span>
-//             <p className="mb-0">+ 123 456 789</p>
-//           </div>
-
-//           <div id="cart-info" className="nav-info align-items-center cart-info d-flex justify-content-between mx-lg-5">
-//             <span className="cart-info__icon mr-lg-3"><i className="fas fa-shopping-cart"></i></span>
-//             <p className="mb-0 text-capitalize"><span id="item-count">2 </span> items - $<span className="item-total">10.49</span></p>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-// );
-
-// export default Navigation;
-
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 
 
 import {Link} from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
-appbar:{
-    backgroundColor: 'white'
-},
-title:{
-    color: "#007bff"
-},
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-  
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-}));
+import './style.css';
 
-export default function Navigation() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = event => {
-    setAnchorEl(event.currentTarget);
-  };
+class Navigation extends Component{
+    state = {
+        shoppingCartElement: null,
+        mobileMenuElement: null,
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+        isShoppingCartOpen: false,
+        isMobileMenuOpen: false,
+    };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = event => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
+    renderShoppingCart = ()=> (
+        <Menu
+          anchorEl={this.state.shoppingCartElement}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          id='shopping-cart'
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={this.state.isShoppingCartOpen}
+          onClose={this.handleMenuClose}
         >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+          <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+          <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+        </Menu>
+      );
 
-  return (
-    <div className={classes.grow}>
-      <AppBar position="static" className={classes.appbar}>
-        <Toolbar>
-          
-          <Typography className={classes.title} variant="h6" noWrap>
-            Bicycle Rental
-          </Typography>
-          
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <Link to='/'> <Button color="primary" className={classes.button}>
-        Home
-      </Button></Link>
-            <Link to='/store'> <Button color="primary" className={classes.button}>
-        Store
-      </Button></Link>
-            
-            
-            
+    renderMobileMenu = ()=> (
+        <Menu
+          anchorEl={this.state.mobileMenuElement}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          id='mobile-menu'
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={this.state.isMobileMenuOpen}
+          onClose={this.handleMenuClose}
+        >
+            <Link to='/'><MenuItem>Home</MenuItem></Link> 
+            <Link to='/bicycles'><MenuItem>Store</MenuItem></Link>
+        </Menu>
+      );
+
+        handleMobileMenuOpen = (event) => {
+            this.setState({
+                mobileMenuElement : event.currentTarget,
+                isMobileMenuOpen : !this.state.isMobileMenuOpen
+            });
+        }
+        handleShoppingCartOpen = (event) => {
+            this.setState({shoppingCartElement : event.currentTarget, isShoppingCartOpen: !this.state.isShoppingCartOpen});
+        }
+        handleMenuClose = () => {
+            this.setState({
+                mobileMenuElement : null,
+                shoppingCartElement: null,
+                isShoppingCartOpen: false,
+                isMobileMenuOpen: false,
+            });   
+        };
+
+      render(){
+        return (
+        
+        <div className="grow">
+            <AppBar position="static" className='appbar'>
+              <Toolbar>
+                
+                <Typography className="appbar-title" variant="h6" noWrap>
+                  Bicycle Rental
+                </Typography>
+                
+                <div className="grow" />
+                <div className="sectionDesktop">
+                  <Link to='/'> <Button className="button">Home</Button></Link>
+                  <Link to='/store'> <Button className="button">Store</Button></Link>
+
+                </div>
+                <IconButton aria-label="show 4 new items in shopping cart" color="inherit"
+                  edge="end"
+                  aria-controls='shopping-cart'
+                  aria-haspopup="true"
+                  onClick={this.handleShoppingCartOpen}>
+                    <Badge badgeContent={4} color="secondary">
+                      <ShoppingCart color='primary'/>
+                    </Badge>
+                  </IconButton>
+                <div className="sectionMobile">
+                  <IconButton
+                    aria-label="show more"
+                    aria-controls='mobile-menu'
+                    aria-haspopup="true"
+                    onClick={this.handleMobileMenuOpen}
+                    color="inherit"
+                  >
+                    <MenuIcon color='primary'/>
+                  </IconButton>
+                </div>
+              </Toolbar>
+            </AppBar>
+            {this.renderMobileMenu()}
+            {this.renderShoppingCart()}
           </div>
-          <IconButton aria-label="show 4 new items in shopping cart" color="inherit"
-            edge="end"
-            aria-controls={menuId}
-            aria-haspopup="true"
-            onClick={handleProfileMenuOpen}>
-              <Badge badgeContent={4} color="secondary">
-                <ShoppingCart color='primary'/>
-              </Badge>
-            </IconButton>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-  );
+        );
+        
+      }
 }
+
+export default Navigation;
