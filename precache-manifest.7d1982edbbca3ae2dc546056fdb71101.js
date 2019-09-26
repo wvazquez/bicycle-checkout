493a60ac7842065674c798e3a094a58a0ae928ba
wvazquez <vazquezwilmer@gmail.com>
2019-09-26 14:06:46 -0400

    Updates

---
+++ precache-manifest.7d1982edbbca3ae2dc546056fdb71101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cd7883d623083e64b69e3f5aa4d6a1",
+    "url": "/bicycle-checkout/index.html"
+  },
+  {
+    "revision": "473874f8b64c62dcc3c9",
+    "url": "/bicycle-checkout/static/css/main.08051335.chunk.css"
+  },
+  {
+    "revision": "77bd574949d12fe73b6c",
+    "url": "/bicycle-checkout/static/js/2.c996d6a9.chunk.js"
+  },
+  {
+    "revision": "473874f8b64c62dcc3c9",
+    "url": "/bicycle-checkout/static/js/main.0d1a5a43.chunk.js"
+  },
+  {
+    "revision": "2199c30e0837e94b5e74",
+    "url": "/bicycle-checkout/static/js/runtime-main.b00e8853.js"
+  }
+]);
