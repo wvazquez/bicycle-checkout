a5bd4cbb46e1307e927ff99a3683337ea9acde32
wvazquez <vazquezwilmer@gmail.com>
2019-09-27 03:12:03 -0400

    Updates

--- precache-manifest.b7f80a83ede23ac04ea01362aa83cf96.js
+++ precache-manifest.b7f80a83ede23ac04ea01362aa83cf96.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a2c2b09c12b8ea59998e0ebcdd39c11",
+    "revision": "b57669568dc1bb05051bfcdc9aee7948",
     "url": "/bicycle-checkout/index.html"
   },
   {
-    "revision": "c8cdf9920e191ae9c243",
+    "revision": "11c852a48b4a632a11d1",
     "url": "/bicycle-checkout/static/css/main.08051335.chunk.css"
   },
   {
-    "revision": "37b6bb40ad9e694cf671",
-    "url": "/bicycle-checkout/static/js/2.ed0e63bb.chunk.js"
+    "revision": "09df59f2ccad07dd26dd",
+    "url": "/bicycle-checkout/static/js/2.f087bd3b.chunk.js"
   },
   {
-    "revision": "c8cdf9920e191ae9c243",
-    "url": "/bicycle-checkout/static/js/main.6a6ae123.chunk.js"
+    "revision": "11c852a48b4a632a11d1",
+    "url": "/bicycle-checkout/static/js/main.02d075b5.chunk.js"
   },
   {
     "revision": "2199c30e0837e94b5e74",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bicycle-checkout/precache-manifest.e28bfc63ad6fc44ef981e56a84d0a120.js"
+  "/bicycle-checkout/precache-manifest.b7f80a83ede23ac04ea01362aa83cf96.js"
 );
 
 self.addEventListener('message', (event) => {
