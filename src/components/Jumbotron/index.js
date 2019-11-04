7646e61207b87eb54ead0610d3c0168c1d53fbc5
wvazquez <vazquezwilmer@gmail.com>
2019-11-04 14:01:22 -0500

    Fixing sizing on smaller screens.

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ import './style.css';
 const Jumbotron = ()=>(
     <div className="jumbotron jumbotron-fluid jumbotron-bicycle">
       <div className="container">
-        <h1 className="text-capitalize">Reserve. Pick-up.<strong className="jumbotron-title ">Explore!</strong></h1>
+        <h1 className="text-capitalize jumbotron-text">Reserve. Pick-up.<br/><strong className="jumbotron-title ">Explore!</strong></h1>
         <Link smooth={true} to="/#store" className="btn jumbotron-link text-uppercase my-2">View rentals</Link>
       </div>
     </div>
